a9afc58bf39ad466a4197b34c6155ed6ddb7844a
Caio Proiete <caio.proiete@gmail.com>
2012-10-11 11:34:37 +0100

    ListView control and Data binding

---
+++ AppCentral/js/data.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="//Microsoft.WinJS.1.0/js/base.js" />
+(function () {
+    "use strict";
+
+    var eventsList = new WinJS.Binding.List();
+
+    eventsList.push({ title: 'Win8 w/ HTML+JS', speakers: 'Caio' });
+    eventsList.push({ title: 'Windows 8 UX Design - Part 1', speakers: 'Leslie' });
+
+    WinJS.Namespace.define("Data", {
+        allEvents : eventsList
+    });
+})();
